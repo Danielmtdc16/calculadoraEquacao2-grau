46d66d1f51c97600e28f7ca4922421bc493dda66
Danielmtdc16 <danieltdc16@gmail.com>
2022-06-30 12:49:00 -0300

    finalizando o script, agora está calculando corretamente

---
+++ script/script.js
@@ -0,0 +1,45 @@
+ function calcular(){
+    // puxar os valores dos coeficientes
+
+    let a = document.getElementById("a").value;
+    let b = document.getElementById("b").value;
+    let c = document.getElementById("c").value;
+
+    // calcular primeiramente o delta da equação
+
+    let r1 = document.getElementById("r1");
+    letr2 = document.getElementById("r2");
+    let informacaoDaEq = document.getElementById("informacaoDaEq");
+
+    r1.innerHTML = "";
+    r2.innerHTML = "";
+    informacaoDaEq.innerHTML = "";
+
+    let delta = Math.pow(parseFloat(b), 2) - 4 * parseFloat(a) * parseFloat(c);
+
+    if (delta < 0){
+        alert("Esta equação não tem soluções reais! Delta menor que 0");
+    }
+
+    else if (delta == 0){
+
+        let x1 = (-(b) + Math.sqrt(delta)) / 2 * a;
+        let x2 = (-(b) - Math.sqrt(delta)) / 2 * a;
+
+        informacaoDaEq.innerHTML = "Delta = 0 --> x1 = x2";
+        r1.innerHTML = "X1 = " + x1.toFixed(4);
+        r2.innerHTML = "X2 = " + x2.toFixed(4);
+
+    }
+
+    else if (delta > 0){
+
+        let x1 = (-(b) + Math.sqrt(delta)) / 2 * a;
+        let x2 = (-(b) - Math.sqrt(delta)) / 2 * a;
+
+        informacaoDaEq.innerHTML = "Delta > 0 --> x1 diferente de x2";
+        r1.innerHTML = "X1 = " + x1.toFixed(4);
+        r2.innerHTML = "X2 = " + x2.toFixed(4);
+
+    }
+ }
